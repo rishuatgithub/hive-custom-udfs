3b179704807c50421089aeaa5d6642fb38a7343b
Rishu <rishu.shrivastava@gmail.com>
2020-08-29 10:59:15 +0100

    Datediif completion + Str occurance refactoring
    Datediff completion
    String occurance
    Test case coverage improved

--- src/main/java/org/apache/hive/custom/udfs/text/FindStrOccurance.java
+++ src/main/java/org/apache/hive/custom/udfs/text/FindStrOccurance.java
@@ -1,8 +1,10 @@
 package org.apache.hive.custom.udfs.text;
 
-import org.apache.commons.lang.StringUtils;
 import org.apache.hadoop.hive.ql.exec.Description;
 import org.apache.hadoop.hive.ql.exec.UDF;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * This is a custom Hive UDFs for searching character/words in a sentence
@@ -11,23 +13,29 @@ import org.apache.hadoop.hive.ql.exec.UDF;
  * @Author: rishu shrivastava
  * @LastUpdated: Aug 22, 2020
  *
- *
  */
-
 @Description(
-    name = "Find Total Occurance of a character in a Word",
-        value = "_FUNC_(str,str) - Return the total number of search character in a word"
+        name = "Find Total Occurance of a character in a Word",
+        value = "_FUNC_(str,str) - Return the total number of search character in a word",
+        extended = "USAGE: select find_str_occurance(\"Hello This is a test function\",\"is\") from table"
 )
 public class FindStrOccurance extends UDF {
 
-    public Integer evaluate(final String word, final String search_word){
+    public Integer evaluate(String word, String search_word){
 
         if(word == null || search_word == null) {
-            return new Integer(0);
+            return null;
         }
 
-        int number_of_occurances = StringUtils.countMatches(word,search_word);
+        int count = 0;
 
-        return new Integer(number_of_occurances);
+        Pattern pattern = Pattern.compile(search_word);
+        Matcher matcher = pattern.matcher(word);
+
+        while (matcher.find()){
+            count += 1;
+        }
+
+        return count;
     }
 }
